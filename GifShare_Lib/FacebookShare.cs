684caa68aa915d59e484cfd263274300ea7f1469
Mason Campbell <WaffleDefend29@gmail.com>
2017-08-07 14:19:57 -0600

    Lats changes made at the end of class

--- GifShare_Lib/FacebookShare.cs
+++ GifShare_Lib/FacebookShare.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace GifShare_Lib
 {
     public class FacebookShare : IPlatformShare
     {
+        private string appID = "";
+        private string appSecret = "";
+
         public void PullSecrets()
         {
-            JObject
+            JObject facebookSecrets = JObject.Parse(File.ReadAllText("../Data/PlatformSecrets.json"));
+
+            appID = facebookSecrets["facebook"]["appid"].ToString();
+            appSecret = facebookSecrets["facebook"]["appsecret"].ToString();
         }
 
         public void ShareGif()
         {
-            throw new NotImplementedException();
+
         }
     }
 }
